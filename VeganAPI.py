8f3a9cd02da350995c5e25c7f2b1796f9e93a68b
Vitor Peçanha <vitor.pecanha@hotmail.com>
2022-03-16 18:55:11 -0300

    Added put method

--- VeganAPI.py
+++ VeganAPI.py
@@ -5,16 +5,42 @@ import pandas as pd
 import ast
 
 class Users(Resource):
-    def get(self):
-        data = pd.read_csv('ApiRestaurants/users.csv')
-        return {'data': data.to_dict()}, 200
 
+    def put(self):
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('userId', required=True)
+        parser.add_argument('location', required=True)
+        args = parser.parse_args()
+
+        data = pd.read_csv('ApiRestaurants/users.csv')
+
+        if args['userId'] in list(data['userId']):
+            data['locations'] = data['locations'].apply(
+                lambda x: ast.literal_eval(x)
+            )
+
+            user_data = data[data['userId'] == args['userId']]
+
+            user_data['locations'] = user_data['locations'].values[0] \
+                .append(args['location'])
+
+            data.to_csv('users.csv', index=False)
+
+            return {'data' : data.to_dict()}, 200
+        
+        else:
+            return{
+                'message' : f"'{args['userId']}' user not found."
+            }, 404
+
+    
     def post(self):
         parser = reqparse.RequestParser()
 
-        parser.add_argument('userId', require=True)
-        parser.add_argument('name', require=True)
-        parser.add_argument('city', require=True)
+        parser.add_argument('userId', required=True)
+        parser.add_argument('name', required=True)
+        parser.add_argument('city', required=True)
 
         args = parser.parse_args()
 
@@ -26,8 +52,19 @@ class Users(Resource):
         })
 
         data = pd.read_csv('ApiRestaurants/users.csv')
-        data = data.append(new_data, ignore_index=True)
-        data.to_csv('users.csv', index=False)
+
+        if args['userId'] in list(data['userId']):
+            return{
+                'message' : f"'{args['userId']}' already exists."
+            }, 401
+        
+        else:
+            data = data.append(new_data, ignore_index=True)
+            data.to_csv('users.csv', index=False)
+            return {'data': data.to_dict()}, 200
+
+    def get(self):
+        data = pd.read_csv('users.csv')
         return {'data': data.to_dict()}, 200
 
 class Locations(Resource):
